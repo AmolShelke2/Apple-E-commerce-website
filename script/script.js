c379bae2c85717db62375d015c47da6d870c5086
Amol shelke <shelkeamol897@gmail.com>
2022-10-02 22:06:40 +0530

    Added Javascript for slideshow animation

--- script/script.js
+++ script/script.js
@@ -63,3 +63,43 @@ document.querySelector(".controls").addEventListener("mouseout", () => {
 });
 
 // End of Cube
+
+// SlideShow
+
+const slideShowDivs = () => {
+  for (let i = 1; i <= 5; i++) {
+    const div = document.createElement("div");
+
+    div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+    i == 1 && div.classList.add("change");
+
+    document.querySelector(".slideshow").appendChild(div);
+  }
+};
+
+slideShowDivs();
+
+const divs = document.querySelectorAll(".slideshow div");
+
+let a = 1;
+
+const slideShow = () => {
+  setInterval(() => {
+    a++;
+
+    const div = document.querySelector(".slideshow .change");
+    div.classList.remove("change");
+
+    if (a < divs.length) {
+      div.nextElementSibling.classList.add("change");
+    } else {
+      divs[0].classList.add("change");
+      a = 1;
+    }
+  }, 2000);
+};
+
+slideShow();
+
+// end of slideshow
