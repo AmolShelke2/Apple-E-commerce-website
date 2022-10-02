63b4d5ce24fbbb3765d71dbdc9055595dd70a14e
Amol shelke <shelkeamol897@gmail.com>
2022-10-02 18:30:54 +0530

    Added common js and adding logic for cube animation

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
 // Common Js
+document
+  .querySelectorAll(".watch-control, .controls a, .iphone-btn")
+  .forEach((control) =>
+    control.addEventListener("click", (e) => {
+      e.preventDefault();
+    })
+  );
+
+// Cube
