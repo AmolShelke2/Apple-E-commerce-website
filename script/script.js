d3c0e51c0a84bfd605d686099a6e58ba983c21e5
Amol shelke <shelkeamol897@gmail.com>
2022-10-04 00:35:57 +0530

    Fixed the Section 1 cube animation rotation

--- script/script.js
+++ script/script.js
@@ -25,25 +25,25 @@ document.querySelector(".bottom-x-control").addEventListener("click", () => {
 });
 
 document.querySelector(".left-y-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${(x -= 20)}deg) rotateZ(${z}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg) `;
 });
 
 document.querySelector(".right-y-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${(x += 20)}deg) rotateZ(${z}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg) `;
 });
 
 document.querySelector(".top-z-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg) `;
 });
 
 document.querySelector(".bottom-z-control").addEventListener("click", () => {
-  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg)`;
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg) `;
 });
 
 const playPause = () => {
   if (bool) {
     interval = setInterval(() => {
-      cube.style.transform = `rotate(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
+      cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
     }, 100);
   } else {
     clearInterval(interval);
