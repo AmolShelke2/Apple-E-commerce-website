c978f8eed5385a52c7c1a01d9baa7a814d0fec54
Amol shelke <shelkeamol897@gmail.com>
2022-10-03 18:52:23 +0530

    Added JavaScript logic for section 3 content scroll animation

--- script/script.js
+++ script/script.js
@@ -103,3 +103,16 @@ const slideShow = () => {
 slideShow();
 
 // end of slideshow
+
+// Section 3
+
+const section3Content = document.querySelector(".section-3-content");
+
+window.addEventListener("scroll", () => {
+  if (
+    window.pageYOffset + window.innerHeight >=
+    section3Content.offsetTop + section3Content.offsetHeight / 2
+  ) {
+    section3Content.classList.add("change");
+  }
+});
